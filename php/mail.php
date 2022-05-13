f7037c437b7280e1d5fc782825f0a41e930e3076
Stepan Derevetskiy <47810314+Stepan77Der@users.noreply.github.com>
2022-05-13 23:46:37 +0300

    .

--- php/mail.php
+++ php/mail.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 'hello';
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method !== 'POST'){
+    exit();
+
+}
+
+$project_name = 'PizzaTime';
+$admin_email = '';
+$form_subject = 'Заявка з сайту';
+$message = 'Hello';
+
+$headers  = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/plain; charset=utf-8\r\n";
+$headers .= "From: $admin_email\r\n";
